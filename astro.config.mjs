706b01eb3a69ecf161479bb164232ec30e1dbfff
njcnki <njcnki@njcnkideMacBook-Pro.local>
2026-03-12 12:49:45 +0800

    Update base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://njcnki.github.io', // 假设你的 GitHub 用户名是 njcnki
-  base: '/',
+  site: 'https://njcnki.github.io', 
+  base: '/my-blog/',
 });
